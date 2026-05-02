6311810605117e7b24139dedf9d61efb12e9d0c0
Arijit Das <arijitdas.das99@gmail.com>
2026-05-02 23:43:38 +0530

    huggingface model added

--- Backend/Core/Features/LLmModelCore/llmService.py
+++ Backend/Core/Features/LLmModelCore/llmService.py
@@ -1,5 +1,6 @@
 # Storing LLM models => LLaMa, Mistral, Gemini
 
+# AI MODEL (GENERATIVE AI MODELS)
 class LLM_SERVICE_PROVIDER:
 
     MODEL_LLAMA = "llama-3.3-70b-versatile"
@@ -7,4 +8,10 @@ class LLM_SERVICE_PROVIDER:
     MODEL_MISTRAL_2 = "mistral-small-latest"
     MODEL_GEMINI = "gemini-2.5-flash-lite"
 
+# EMBEDDING MODELS BY HUGGINGFACE
+class EMBEDDING_MODEL_PROVIDER:
+
+    HUGGING_FACE_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
+
+embedding_model_provider = EMBEDDING_MODEL_PROVIDER()
 llm_service_provider = LLM_SERVICE_PROVIDER()
