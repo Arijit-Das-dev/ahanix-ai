22765a423d28cb0b6ebc8a6e4f410b0d58ac5eb4
Arijit Das <arijitdas.das99@gmail.com>
2026-05-03 10:36:31 +0530

    updated cohere

--- Backend/Services/modelCohere.py
+++ Backend/Services/modelCohere.py
@@ -1,3 +1,8 @@
+import os
+from Backend.Config.settings import settings # cohere api key
+from Backend.Core.Features.LLmModelCore.llmService import llm_service_provider # cohere model
+
+
 class MODEL_COHERE:
     
     def __init__(self):
